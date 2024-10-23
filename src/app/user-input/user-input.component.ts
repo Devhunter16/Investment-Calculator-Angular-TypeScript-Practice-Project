014cb4e220c1945fb4ca80b1b7356f5d1ee29cb0
Devin Hunter <devhunter16@gmail.com>
2024-10-23 09:09:19 -0700

    Tinkering around with signals and tryingn to emit a value from the user-input file.

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, effect, input, output, computed } from '@angular/core';
 
 @Component({
   selector: 'app-user-input',
@@ -9,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class UserInputComponent {
 
-}
+  // Setting these variables as signal inputs to the component
+  initialInvestment = input.required<number>();
+  annualInvestment = input.required<number>();
+  expectedReturn = input.required<number>();
+  duration = input.required<number>();
+
+  userInputArray = output<number[]>();
+
+  constructor() {
+    effect(() => {
+      console.log("User Inputs: " + this.userInputs());
+    });
+  };
+
+  userInputs = computed<number[]>(() => {
+    return [
+      this.initialInvestment(),
+      this.annualInvestment(),
+      this.expectedReturn(),
+      this.duration()
+    ];
+  });
+
+  onCalculate() {
+    this.userInputArray.emit(this.userInputs());
+  };
+};
