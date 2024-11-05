2ebe49f0582d7cccab7812052d18a04d3501e737
Devin Hunter <devhunter16@gmail.com>
2024-11-05 09:25:10 -0700

    Adding te currency pipe in order to appropriately format the data in the table.

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,18 +1,15 @@
 import { Component, input } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
 
 import { type InvestmentResult } from '../../models/investment-result.model';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
 export class InvestmentResultsComponent {
-
   investmentResults = input.required<InvestmentResult[] | undefined>();
-
-  //console.log("Hello from investment results!");
-
 };
